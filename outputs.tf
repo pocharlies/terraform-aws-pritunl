7aaf0db73e4a7c4c81cad85259de14eacba32514
Daniel Ibáñez <daniel.ibanez@invoicesharing.com>
2021-02-04 18:33:14 +0100

    Fix incorrect epel installed

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,27 @@
 output "pritunl_private_ip" {
-  value = "${aws_instance.pritunl.private_ip}"
+  value = aws_instance.pritunl.private_ip
 }
 
 output "pritunl_public_ip" {
-  value = "${aws_instance.pritunl.public_ip}"
+  value = aws_eip.pritunl.public_ip
 }
 
 output "security_group_ids" {
-  value = ["${aws_security_group.pritunl.id}",  "${aws_security_group.allow_from_office.id}"]
+  value = [aws_security_group.pritunl.id,  aws_security_group.allow_from_office.id]
 }
 
 output "main_security_group_id" {
-  value = "${aws_security_group.pritunl.id}"
+  value = aws_security_group.pritunl.id
 }
 
 output "office_security_group_id" {
-  value = "${aws_security_group.allow_from_office.id}"
+  value = aws_security_group.allow_from_office.id
 }
 
 output "aws_instance_id" {
-  value = "${aws_instance.pritunl.id}"
+  value = aws_instance.pritunl.id
 }
 
 output "aws_ami_id" {
-  value = "${data.aws_ami.aws.id}"
+  value = data.aws_ami.aws.id
 }
--- provision.sh
+++ provision.sh
@@ -31,7 +31,6 @@ gpgcheck=1
 enabled=1
 EOF
 
-sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A > key.tmp; sudo rpm --import key.tmp; rm -f key.tmp
 sudo yum -y install pritunl mongodb-org
